44de3b7391a670c0068183a97b212ab2a0397e1d
Sanjeev <sanju.jyothikrishnan@gmail.com>
2024-03-25 18:13:25 +0530

    PassShaper code check in

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PasswordGeneratorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/password-generator/password-generator.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+@Component({
+  selector: 'app-password-generator',
+  templateUrl: './password-generator.component.html',
+  styleUrl: './password-generator.component.css'
+})
+export class PasswordGeneratorComponent {
+  password: string = '';
+  passwordLength: number = 12; // default password length
+  includeDigits: boolean = true; // include digits by default
+  includeSpecialChars: boolean = true; // include special characters by default
+  includeUpperCaseLetter: boolean = true; // include
+  includeLowerCaseLetter: boolean = true; // include
+
+  constructor(private clipboard: Clipboard) {}
+  generatePassword(): void {
+    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const digits = '0123456789';
+    const symbols = '!@#$%^&*()_+{}[]';
+
+
+    if(!(this.includeDigits) && !(this.includeLowerCaseLetter) && !(this.includeSpecialChars)&& !(this.includeUpperCaseLetter))
+    {
+      alert('Select any one checkbox');
+      this.password='';
+    }
+    else{
+      let characters = '';
+      if (this.includeUpperCaseLetter) characters += uppercaseLetters;
+      if (this.includeLowerCaseLetter) characters += lowercaseLetters;
+      if (this.includeDigits) characters += digits;
+      if (this.includeSpecialChars) characters += symbols;
+  
+      let newPassword = '';
+  
+      for (let i = 0; i < this.passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        newPassword += characters[randomIndex];
+      }
+  
+      this.password = newPassword;
+    }
+    }
+    
+    
+  copyToClipboard(): void {
+    if (this.password) {
+      this.clipboard.copy(this.password); // Copy password to clipboard
+    }
+  }
+}
+
